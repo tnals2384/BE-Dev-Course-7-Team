7ae7252982bab0f00214c8ee646112a60386df67
a-young0210 <dkdudab@hanmail.net>
2024-08-01 15:15:22 +0900

    clothesTest CRUD

---
+++ Clothes.java
@@ -0,0 +1,53 @@
+package day0731.shopAY;
+
+public class Clothes {
+    private int id;
+    private String name;
+    private int price;
+    private String category;
+
+    public Clothes(int id, String name, int price, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return id + "\t" + name + "\t" + price + "\t" + category;
+    }
+}
